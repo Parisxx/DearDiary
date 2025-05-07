a73ee2bc52303b94c4973b1a100dec2f7dbc02c8
Parisxx <parisstassen@gmail.com>
2025-05-07 12:49:48 +0200

    feat: added settings page

---
+++ settings.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>Dear Diary</title>
+    <link rel="icon" type="image/x-icon" href="src/img/logo.png">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    
+</body>
+</html>
